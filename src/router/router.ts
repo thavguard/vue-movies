bd83fe1a75c2be796976d0d6b84e7f9e864b81b1
Sergey Kuspekov <kuspekov20@yandex.ru>
2022-12-08 17:04:03 +0700

    удалил начальные файлы

---
+++ src/router/router.ts
@@ -0,0 +1,9 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home/Home.vue";
+
+const routes = [{ path: "/", name: "home", component: Home }];
+
+export const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+});
---
+++ src/views/Home/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="">home component</div>
+</template>

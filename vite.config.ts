1aed9a889a7a0ab6b18ce9d269c96d5cfc20100a
Sergey Kuspekov <kuspekov20@yandex.ru>
2022-12-27 14:30:44 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       registerType: "autoUpdate",
       injectRegister: "auto",
       devOptions: {
-        enabled: true,
+        enabled: false,
       },
     }),
   ],

17ba48cf670937645a09323069e146eba6cd8206
Sergey Kuspekov <kuspekov20@yandex.ru>
2022-12-27 14:28:32 +0700

    создал pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
